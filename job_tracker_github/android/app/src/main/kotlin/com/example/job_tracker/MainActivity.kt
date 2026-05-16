9e5b7ce7fbf7beeab5e2f2f2a2eff975fd5204c9
NOV-NIKO <165112974+NOV-NIKO@users.noreply.github.com>
2026-05-16 21:07:28 +0800

    Delete job_tracker_github directory

--- job_tracker_github/android/app/src/main/kotlin/com/example/job_tracker/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.job_tracker
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- job_tracker_github/lib/config/config.dart
+++
@@ -1,7 +0,0 @@
-class Config {
-  static const String glmApiKey = 'YOUR_GLM_API_KEY_HERE';
-  
-  static void initialize() {
-    OCRHelper.apiKey = glmApiKey;
-  }
-}
